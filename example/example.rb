1dcab725761664751991496964774d9b72fcf7b6
Chris Lowis <chris.lowis@gmail.com>
2011-07-13 14:29:27 +0100

    Example of how to use the library

---
+++ example/example.rb
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+require_relative '../lib/tf-idf'
+
+# First we define a corpus to hold the documents
+corpus = Corpus.new
+
+# Now we add documents to the corpus. Here we're using a few headlines
+# from the BBC News RSS feed.
+
+headlines = [
+             "Broad powers for hacking inquiry",
+             "UK unemployment level falls again",
+             "NI riots leads to 26 arrests",
+             "IMF urges spending cuts in Italy",
+             "UK ticket wins £161m Euromillions",
+             "EU proposal to save fish stocks",
+             "Pilots cleared by Chinook report",
+             "Briton dies in Greece 'stabbing'",
+             "'Spy' committee calls for reforms",
+             "UK charities step up Somalia aid"
+            ]
+
+documents = []
+headlines.each do |headline|
+  # create a document object from the headline
+  document = Document.new(headline)
+
+  # store the document object for later
+  documents << document
+
+  # add the document to the corpus
+  corpus << document
+end
+
+# Print a list of unique terms extracted from the documents
+puts corpus.terms
+
+# Calculate the similarity between two documents
+puts corpus.similarity(documents[1], documents[4])
+
