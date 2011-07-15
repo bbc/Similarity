e587c757b2bb2ae0d04224a0ee83f08cae7ce76d
Chris Lowis <chris.lowis@gmail.com>
2011-07-15 11:48:53 +0100

    change require paths

--- lib/tf-idf.rb
+++ lib/tf-idf.rb
@@ -1,3 +1,3 @@
-require_relative './tf-idf/corpus.rb'
-require_relative './tf-idf/document.rb'
-require_relative './tf-idf/vector_calculations.rb'
+require 'tf-idf/corpus'
+require 'tf-idf/document'
+require 'tf-idf/vector_calculations'
