9c5387a3c4f4e451914226fa39cb29aaa8ee3c6f
Chris Lowis <chris.lowis@gmail.com>
2011-08-31 17:16:53 +0100

    optimize an inner loop in the construction of TermDocumentMatrix

--- lib/similarity/term_document_matrix.rb
+++ lib/similarity/term_document_matrix.rb
@@ -6,10 +6,11 @@ class TermDocumentMatrix
   def initialize(corpus)
     @matrix = GSL::Matrix.alloc(corpus.terms.size, corpus.document_count)
 
-    corpus.documents.each_with_index do |document, document_index|
-      corpus.terms.each_with_index do |term, term_index|
-        term = term.first
-        idf = corpus.inverse_document_frequency(term)
+    corpus.terms.each_with_index do |term, term_index|
+      term = term.first
+      idf = corpus.inverse_document_frequency(term)
+
+      corpus.documents.each_with_index do |document, document_index|
         weight = document.term_frequency(term) * idf
         @matrix[term_index, document_index] = weight
       end
